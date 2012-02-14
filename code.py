26177aef61b464843c39abe0289e158e27c09ea1
Mehul Ved <mehul.n.ved@gmail.com>
2012-02-15 03:09:53 +0530

    Code to display emoticon and it's meaning now works

--- code.py
+++ code.py
@@ -9,7 +9,14 @@ app = web.application(urls, globals())
 
 class index:
     def GET(self, smiley):
-        return render.index(smiley)
+        result={} # let's define an empty dictionary which will store the returned results
+        # Match the smiley from the emoticon.txt file and store the results in the dictionary
+        for line in open('emoticon.txt'):
+            if smiley in line:
+                key,value=line.split('\t')
+		if key==smiley: # to get an exact match
+                    result[key]=value
+        return render.index(result) # send the result to the template index
 
 if __name__ == "__main__": 
     app.run()
